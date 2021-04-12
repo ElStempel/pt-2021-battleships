5fb66673ae666b9c68d9effa07410471ea6a79c9
ElStempel <steplewski.aleks@gmail.com>
2021-04-12 14:39:25 +0200

    updated stats and users routing

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -4,12 +4,32 @@ var User = require('../models/user')
 
 /* GET stats listing. */
 router.get('/', function(req, res, next) {
-  res.send('Stats routing');
+  res.status(200).send('Stats routing');
 });
 
 // UPDATE STATS
 router.patch('/update', async function(req, res, next) {
-  res.send('Updating stats');
+  let user_check = await User.findOne({_id: req.body._id});
+  if (user_check){
+    //console.log(user_check)
+    user_check.stats.games_played += req.body.stats.games_played;
+    user_check.stats.ships_sunk += req.body.stats.ships_sunk;
+    user_check.stats.ships_lost += req.body.stats.ships_lost;
+    user_check.stats.shots_fired += req.body.stats.shots_fired;
+    try {
+      await user_check.save()
+      var table = []
+      table.push({
+        'user_name': user_check.user_name,
+        'stats': user_check.stats
+      })
+      res.status(200).send(table);
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  } else {
+    return res.status(400).send("User doesn't exist");
+  }
 });
 
 function new_Table(json_table){
@@ -26,7 +46,7 @@ function new_Table(json_table){
 router.get('/check', async function(req, res, next) {
   let all = await User.find()
   let tabela = await new_Table(all)
-  res.send(tabela);
+  res.status(200).send(tabela);
 });
 
 router.post('/mine', async function(req, res, next) {
@@ -37,7 +57,7 @@ router.post('/mine', async function(req, res, next) {
       'user_name': user_check.user_name,
       'stats': user_check.stats
     })
-    return res.send(table) 
+    return res.status(200).send(table) 
   } else {
     res.status(400).send("User doesn't exist")
   }
