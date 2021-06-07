181a0d5e932fe63202d4827ed8fa895b6ef4e065
ElStempel <steplewski.aleks@gmail.com>
2021-06-07 16:46:05 +0200

    attack3

--- server/routes/games.js
+++ server/routes/games.js
@@ -430,6 +430,99 @@ router.post('/shot/torpedo', async function(req, res, next) {
     }
 });
 
+router.post('/shot/airstrike', async function(req, res, next) {
+    let game = await Game.findOne({_id: req.body.game_id})
+    if(game){
+        if(game.p1_ready == true && game.p2_ready == true){
+            if(game.player_1 == req.body.player_id){
+                if(game.turn == 1){
+                    if(req.body.coordinates.x -1 >= 0 && req.body.coordinates.x +1 < game.map_size 
+                        && req.body.coordinates.y -1 >= 0 && req.body.coordinates.y +1 < game.map_size){
+                        var x = req.body.coordinates.x;
+                        var y = req.body.coordinates.y;
+                        for (var x_s = x-1; x_s <= x+1; x_s++){
+                            for (var y_s = y-1; y_s <= y+1; y_s++){
+                                if(parseInt(game.p2_map[x_s][y_s]/10) > 0){
+                                    game.p2_map[x_s][y_s]+=1;
+                                    snd_info = await search_and_destroy(game.p2_map, game.p2_map[x_s][y_s], game.map_size)
+                                    game.p2_map = snd_info[0];
+                                    if(snd_info[1] == true){
+                                        game.p1.ships_sunk += 1;
+                                        game.p2.ships_lost += 1;
+                                        if(game.p1.ships_sunk == 5){
+                                            game.winner = 1;
+                                        }
+                                    }
+                                } else {
+                                    game.p2_map[x_s][y_s]=5;
+                                }
+                            }
+                        }
+                        game.turn = 2;
+                        try {
+                            game.p1.attack3 = false
+                            await game.markModified("p2_map")
+                            await game.save()
+                            res.status(200).send('Shot registered');
+                        } catch (error) {
+                            res.status(500).send(error)
+                        }
+                    } else {
+                        res.status(409).send('Coordinates out of range')
+                    }
+                } else {
+                    res.status(405).send("This is other player's turn")
+                }
+            } else if(game.player_2 == req.body.player_id){
+                if(game.turn == 2){
+                    if(req.body.coordinates.x -1 >= 0 && req.body.coordinates.x +1 < game.map_size 
+                        && req.body.coordinates.y -1 >= 0 && req.body.coordinates.y +1 < game.map_size){
+                        var x = req.body.coordinates.x;
+                        var y = req.body.coordinates.y;
+                        for (var x_s = x-1; x_s <= x+1; x_s++){
+                            for (var y_s = y-1; y_s <= y+1; y_s++){
+                                if(parseInt(game.p1_map[x_s][y_s]/10) > 0){
+                                    game.p1_map[x_s][y_s]+=1;
+                                    snd_info = await search_and_destroy(game.p1_map, game.p1_map[x_s][y_s], game.map_size)
+                                    game.p1_map = snd_info[0];
+                                    if(snd_info[1] == true){
+                                        game.p2.ships_sunk += 1;
+                                        game.p1.ships_lost += 1;
+                                        if(game.p2.ships_sunk == 5){
+                                            game.winner = 2;
+                                        }
+                                    }
+                                } else {
+                                    game.p1_map[x_s][y_s]=5;
+                                }
+                            }
+                        }
+                        game.turn = 1;
+                        try {
+                            game.p2.attack3 = false
+                            await game.markModified("p1_map")
+                            await game.save()
+                            res.status(200).send('Shot registered');
+                        } catch (error) {
+                            res.status(500).send(error)
+                        }
+                    } else {
+                        res.status(409).send('Coordinates out of range')
+                    }
+                } else {
+                    res.status(405).send("This is other player's turn")
+                }
+            } else {
+                res.status(405).send("This player is not in this game")
+            }
+        } else {
+            res.status(405).send("Players are not ready yet")
+        }
+    } else {
+        res.status(404).send("Game does not exist")
+    }
+});
+
 function censoreMap(map, map_size){
     for(var i = 0; i < map_size; i++){
         for(var j = 0; j < map_size; j++){
