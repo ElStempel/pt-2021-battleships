300831d811bb3ae25eb24ab996348a93b3f8a7d0
KonyPL <mrkony98@gmail.com>
2021-06-09 02:22:01 +0200

    Custom shots for bot

--- server/routes/games.js
+++ server/routes/games.js
@@ -132,9 +132,173 @@ async function bot_shot(game){
     }
 }
 
+async function bot_torpedo(game){
+    var bot_x;
+    var bot_y;
+    var bot_orientation = getRandomInt(0, 100)
+
+    if(bot_orientation > 50){
+
+        bot_x = getRandomInt(0, game.map_size)
+        for (var y = 0; y < game.map_size; y++){
+            if(parseInt(game.p1_map[y][bot_x]/10) > 0){
+                game.p1_map[y][bot_x]+=1;
+                snd_info = await search_and_destroy(game.p1_map, game.p1_map[y][bot_x], game.map_size)
+                game.p1_map = snd_info[0];
+                if(snd_info[1] == true){
+                    game.p2.ships_sunk += 1;
+                    game.p1.ships_lost += 1;
+                    if(game.p2.ships_sunk == 5){
+                        game.winner = 2;
+                    }
+                }
+            } else {
+                game.p1_map[y][bot_x]=5;
+            }
+        }
+
+    } else {
+        bot_y = getRandomInt(0, game.map_size)
+        for (var x = 0; x < game.map_size; x++){
+            if(parseInt(game.p1_map[bot_y][x]/10) > 0){
+                game.p1_map[bot_y][x]+=1;
+                snd_info = await search_and_destroy(game.p1_map, game.p1_map[bot_y][x], game.map_size)
+                game.p1_map = snd_info[0];
+                if(snd_info[1] == true){
+                    game.p2.ships_sunk += 1;
+                    game.p1.ships_lost += 1;
+                    if(game.p2.ships_sunk == 5){
+                        game.winner = 2;
+                    }
+                }
+            } else {
+                game.p1_map[bot_y][x]=5;
+            }
+        }
+    }
+    game.turn = 1;
+    game.p2.attack1 = false;
+    try {
+        await game.markModified("p1_map")
+        await game.save()
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+async function bot_cluster(game){
+    var bot_x;
+    var bot_y;
+    var can_shoot = true;
+    while(can_shoot){
+        bot_x = getRandomInt(0, game.map_size)
+        bot_y = getRandomInt(0, game.map_size)
+        //Check if chosen place was never shot before
+        if(game.p1_map[bot_y][bot_x]%10 == 0){
+            //Check if it's a ship or not
+            if(parseInt(game.p1_map[bot_y][bot_x]/10) > 0){
+                game.p1_map[bot_y][bot_x]+=1;
+                bot_snd_info = await search_and_destroy(game.p1_map, game.p1_map[bot_y][bot_x], game.map_size)
+                game.p1_map = bot_snd_info[0];
+                if(bot_snd_info[1] == true){
+                    game.p2.ships_sunk += 1;
+                    game.p1.ships_lost += 1;
+                    if(game.p2.ships_sunk == 5){
+                        game.winner = 2;
+                    }
+                }
+                game.turn = 1;
+                game.p2.attack2 = false;
+                can_shoot = false;
+            } else {
+                game.p2.shots_missed += 1;
+                game.p1_map[bot_y][bot_x]=5;
+            }
+            try {
+                await game.markModified("p1_map")
+                await game.save()
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+}
+
+async function bot_airstrike(game){
+    var bot_x;
+    var bot_y;
+    var tries = 3;
+    var can_shoot = true;
+    while(can_shoot){
+        bot_x = getRandomInt(0, game.map_size)
+        bot_y = getRandomInt(0, game.map_size)
+        
+        if(bot_x -1 >= 0 && bot_x +1 < game.map_size 
+            && bot_y -1 >= 0 && bot_y +1 < game.map_size){
+            //Check if chosen place was never shot before up to 3 times
+            if((game.p1_map[bot_y][bot_x]%10 == 0 && game.p1_map[bot_y-1][bot_x-1]%10 == 0 &&
+                game.p1_map[bot_y-1][bot_x]%10 == 0 && game.p1_map[bot_y-1][bot_x+1]%10 == 0 &&
+                game.p1_map[bot_y][bot_x-1]%10 == 0 && game.p1_map[bot_y][bot_x+1]%10 == 0 &&
+                game.p1_map[bot_y+1][bot_x-1]%10 == 0 && game.p1_map[bot_y+1][bot_x]%10 == 0 &&
+                game.p1_map[bot_y+1][bot_x+1]%10 == 0) || tries < 0){
+                //Check if it's a ship or not
+                for (var x_s = bot_x-1; x_s <= bot_x+1; x_s++){
+                    for (var y_s = bot_y-1; y_s <= bot_y+1; y_s++){
+                        if(parseInt(game.p1_map[y_s][x_s]/10) > 0){
+                            game.p1_map[y_s][x_s]+=1;
+                            snd_info = await search_and_destroy(game.p1_map, game.p1_map[y_s][x_s], game.map_size)
+                            game.p1_map = snd_info[0];
+                            if(snd_info[1] == true){
+                                game.p2.ships_sunk += 1;
+                                game.p1.ships_lost += 1;
+                                if(game.p2.ships_sunk == 5){
+                                    game.winner = 1;
+                                }
+                            }
+                        } else {
+                            game.p1_map[y_s][x_s]=5;
+                        }
+                    }
+                }
+                game.turn = 1;
+                game.p2.attack3 = false;
+                can_shoot = false
+                try {
+                    await game.markModified("p1_map")
+                    await game.save()
+                } catch (error) {
+                    console.log(error)
+                }
+            } else {
+                tries--;
+            }
+            
+        }
+    }
+}
+
 async function bot_play(game){
-    //if(){}
-    bot_shot(game)
+    let room = await Room.findById(game.room)
+    if(room.custom_rules.enabled){
+
+        var probability = getRandomInt(0, 100);
+
+        if(game.p2.attack1 && probability < 10){
+            bot_torpedo(game)
+
+        } else if(game.p2.attack2 && probability >= 10 && probability < 20){
+            bot_cluster(game)
+
+        } else if(game.p2.attack3 && probability >= 20 && probability < 30){
+            bot_airstrike(game)
+
+        } else {
+            bot_shot(game)
+        }
+    } else {
+        bot_shot(game)
+    }  
 }
 
 //shot (game_id, player_id, coordinates:{x, y})
@@ -266,6 +430,11 @@ router.post('/shot/cluster', async function(req, res, next) {
                             } catch (error) {
                                 res.status(500).send(error)
                             }
+
+                            //BOT
+                            if(game.player_2 == null && game.turn == 2){
+                                await bot_play(game)
+                            }
                         } else {
                             res.status(409).send('Coordinates out of range')
                         }
@@ -361,6 +530,11 @@ router.post('/shot/torpedo', async function(req, res, next) {
                             } catch (error) {
                                 res.status(500).send(error)
                             }
+
+                            //BOT
+                            if(game.player_2 == null && game.turn == 2){
+                                await bot_play(game)
+                            }
                         } else if(req.body.coordinates.y >= 0 && req.body.coordinates.y < game.map_size){
                             var y = req.body.coordinates.y;
                             for (var x = 0; x < game.map_size; x++){
@@ -387,6 +561,11 @@ router.post('/shot/torpedo', async function(req, res, next) {
                                 res.status(200).send('Shot registered');
                             } catch (error) {
                                 res.status(500).send(error)
+                            }
+
+                            //BOT
+                            if(game.player_2 == null && game.turn == 2){
+                                await bot_play(game)
                             }
                         } else {
                             res.status(409).send('Coordinates out of range')
@@ -510,6 +689,11 @@ router.post('/shot/airstrike', async function(req, res, next) {
                             res.status(200).send('Shot registered');
                         } catch (error) {
                             res.status(500).send(error)
+                        }
+
+                        //BOT
+                        if(game.player_2 == null && game.turn == 2){
+                            await bot_play(game)
                         }
                     } else {
                         res.status(409).send('Coordinates out of range')
